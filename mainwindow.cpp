78c779d267e930b99be876b5df2bbceb961a24ad
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-05-10 13:34:04 +0800

    Use self defined graphic view widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,7 +5,14 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->graphicsView_main->setScene(&m_default_scene);
+
+    ///< the main draw window settings.
+    ui->tabWidget->setMovable(true);
+    ui->tabWidget->setTabsClosable(true);
+    ui->tabWidget->removeTab(0);
+    ui->tabWidget->insertTab(0, gv, "Main drawing pad");
+    gv->setScene(&m_default_scene);
+    // ui->graphicsView_main->setScene(&m_default_scene);
 }
 
 MainWindow::~MainWindow()
